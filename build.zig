40bf8b080e4c76cd71eaaf8468ddd8189b0ec709
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2026-05-03 15:37:25 +0300

    add library artifact to build

--- build.zig
+++ build.zig
@@ -5,17 +5,19 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("hayal", .{
-        // The root source file is the "entry point" of this module. Users of
-        // this module will only be able to access public declarations contained
-        // in this file, which means that if you have declarations that you
-        // intend to expose to consumers that were defined in other files part
-        // of this module, you will have to make sure to re-export them from
-        // the root file.
         .root_source_file = b.path("src/root.zig"),
-        // Later on we'll use this module as the root module of a test executable
-        // which requires us to specify a target.
         .target = target,
     });
+
+    const lib = b.addLibrary(.{ .name = "hayal", .root_module = mod, .linkage = .dynamic });
+    b.installArtifact(lib);
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
 
     // We need to register each example directory as a module for ZLS to detect their dependencies
     const io = b.graph.io;
@@ -45,53 +47,14 @@ pub fn build(b: *std.Build) void {
                 },
             }),
         });
-
-        // This declares intent for the executable to be installed into the
-        // install prefix when running `zig build` (i.e. when executing the default
-        // step). By default the install prefix is `zig-out/` but can be overridden
-        // by passing `--prefix` or `-p`.
         b.installArtifact(exe);
 
-        // This creates a top level step. Top level steps have a name and can be
-        // invoked by name when running `zig build` (e.g. `zig build run`).
-        // This will evaluate the `run` step rather than the default step.
-        // For a top level step to actually do something, it must depend on other
-        // steps (e.g. a Run step, as we will see in a moment).
         const run_step = b.step("run", "Run the app");
-
-        // This creates a RunArtifact step in the build graph. A RunArtifact step
-        // invokes an executable compiled by Zig. Steps will only be executed by the
-        // runner if invoked directly by the user (in the case of top level steps)
-        // or if another step depends on it, so it's up to you to define when and
-        // how this Run step will be executed. In our case we want to run it when
-        // the user runs `zig build run`, so we create a dependency link.
         const run_cmd = b.addRunArtifact(exe);
         run_step.dependOn(&run_cmd.step);
-
-        // By making the run step depend on the default step, it will be run from the
-        // installation directory rather than directly from within the cache directory.
         run_cmd.step.dependOn(b.getInstallStep());
-
-        // This allows the user to pass arguments to the application in the build
-        // command itself, like this: `zig build run -- arg1 arg2 etc`
         if (b.args) |args| {
             run_cmd.addArgs(args);
         }
     }
-
-    // Creates an executable that will run `test` blocks from the provided module.
-    // Here `mod` needs to define a target, which is why earlier we made sure to
-    // set the releative field.
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    // A run step that will run the test executable.
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
-    // make the two of them run in parallel.
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
 }
