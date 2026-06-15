737d96fed6cb8cfe448e9231f880925de32d7aa7
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2026-06-15 22:59:41 +0300

    add cglm as math library

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/sh
 
-clang src/main.c vendor/glad/glad.c --std=c99 -Wall -Werror -fsanitize=address -I./vendor -lX11 -lGLX -lm -lXrandr -ldl -lpthread -lm -o my_game
+clang src/main.c vendor/glad/glad.c \
+  --std=c99 -Wall -Werror -fsanitize=address \
+  -isystem ./vendor \
+  -lX11 -lGLX -lm -lXrandr -ldl -lpthread -lm \
+  -o my_game
