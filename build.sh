581c810c84725076ca72990395b6460ae3762887
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2026-06-21 18:16:12 +0300

    remove redundant lpthread

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 clang src/main_linux.c vendor/glad/glad.c \
   --std=c99 -Wall -Werror -fsanitize=address \
   -isystem ./vendor \
-  -lX11 -lGLX -lm -lXrandr -ldl -lpthread \
+  -lX11 -lGLX -lm -lXrandr -ldl \
   -D_DEBUG \
   -o my_game
